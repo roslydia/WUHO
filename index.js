b746b1beec6724c1ed87ff25101d54ecd6a5d1ae
roslydia <122244543+roslydia@users.noreply.github.com>
2023-01-09 05:44:43 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login('YOUR_BOT_TOKEN_HERE');
